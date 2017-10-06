aaf6bc09023e678a065b2d79d06af706db3067d5
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-06 03:14:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,9 @@ def monopoly_with_fourth_tier
       pennsylvania_railroad: {
         "mortgage_value" => "$200"
       },
-      b_and_o_railroad: {},
+      b_and_o_railroad: {
+        "mortgage_value" => "$400"
+      },
       shortline: {}
     },
 		rent_in_dollars: {
